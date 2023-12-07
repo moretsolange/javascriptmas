3728fe1840e1d7f7a80a37cbedbdbf57104f4513
brittythed00m <moretimea@outlook.com>
2023-12-07 21:46:17 +0100

    js quick fix

--- day06/index.js
+++ day06/index.js
@@ -4,11 +4,27 @@ const generatedPeople = [];
 let i = 0;
 
 function generateSecretSantaPairs(arr) {
-  let output = {};
+    let output = {};
 
   while (i < arr.length) {
-    let actual = arr[i];
-    let generate = arr[Math.floor(Math.random() * arr.length)];
+      let actual = arr[i];
+      let generate = arr[Math.floor(Math.random() * arr.length)];
+
+      if(actual === generate || generatedPeople.includes(generate)) {
+          continue;
+      }
+
+      // we must force to select the last member if we did not select him/her previously
+      // because otherwise he/she would be left alone for himself/herself (it would result an infinite loop)
+      let last = arr[arr.length - 1];
+      let penultimate = arr[arr.length - 2];
+      if (actual === penultimate && !generatedPeople.includes(last)) {
+          generate = last;
+      }
+
+      generatedPeople.push(generate)
+      output[actual] = generate;
+      i++;
   }
   return output;
 }
